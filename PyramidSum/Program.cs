79122a6fedbd8e9547c69d81779055dfe8bd8b88
BSN <birgitte.strate@gmail.com>
2020-11-26 14:50:05 +0100

    First commit

---
+++ PyramidSum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PyramidSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pyramidDef1 = new List<string>
+            {              
+                "1",     
+                "8 9",   
+                "1 5 9",  
+                "4 5 2 3"
+            };
+            var pyramid1 = new Pyramid(pyramidDef1);
+            string pathMax1;
+            var maxSum1 = pyramid1.FindMaxSum(out pathMax1);
+            Console.WriteLine("**Pyramid 1**");
+            Console.WriteLine("Max sum: " + maxSum1);
+            Console.WriteLine("Path: " + pathMax1);
+
+            var pyramidDef2 = new List<string>
+            {
+                "215",
+                "192 124",
+                "117 269 442",
+                "218 836 347 235",
+                "320 805 522 417 345",
+                "229 601 728 835 133 124",
+                "248 202 277 433 207 263 257",
+                "359 464 504 528 516 716 871 182",
+                "461 441 426 656 863 560 380 171 923",
+                "381 348 573 533 448 632 387 176 975 449",
+                "223 711 445 645 245 543 931 532 937 541 444",
+                "330 131 333 928 376 733 017 778 839 168 197 197",
+                "131 171 522 137 217 224 291 413 528 520 227 229 928",
+                "223 626 034 683 839 052 627 310 713 999 629 817 410 121",
+                "924 622 911 233 325 139 721 218 253 223 107 233 230 124 233"
+            };
+            var pyramid2 = new Pyramid(pyramidDef2);
+            string pathMax2;
+            var maxSum2 = pyramid2.FindMaxSum(out pathMax2);
+            Console.WriteLine("**Pyramid 2**");
+            Console.WriteLine("Max sum: " + maxSum2);
+            Console.WriteLine("Path: " + pathMax2);
+        }
+    }
+}
---
+++ PyramidSum/Pyramid.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PyramidSum
+{
+    public class Pyramid
+    {
+        List<string[]> PyramidList { get; set; }
+        public Pyramid(List<string> pyramidDef)
+        {
+            PyramidList = new List<string[]>();
+            for (int i = 0; i < pyramidDef.Count; i++)
+            {
+                PyramidList.Add(pyramidDef[i].Split(' '));
+            }
+        }
+
+        public int FindMaxSum(out string pathMax)
+        {
+            int maxSum = 0;
+            pathMax = "";
+            string path;
+            int pathCount =(int)Math.Truncate(Math.Pow(2, PyramidList.Count - 1));
+            for (int pathNo = 0; pathNo < pathCount; pathNo++)
+            {
+                int sum = Sum(pathNo, out path);
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                    pathMax = path;
+                }
+            }
+            return maxSum;
+        }
+        private int Sum(int pathNo, out string path)
+        {
+            byte[] bitArr = CreateBitArray(pathNo, PyramidList.Count - 1);
+            path = PyramidList[0][0];
+            int firstValue = Int32.Parse(PyramidList[0][0]);
+            int sum = firstValue;            
+            int value;
+            int valueOld = firstValue;
+            int row = 0;
+            int col = 0;
+            for (int i = 0; i < PyramidList.Count - 1; i++)
+            {
+                if (bitArr[i] == 0)
+                    row++;
+                else
+                {
+                    row++;
+                    col++;
+                }
+                value = Int32.Parse(PyramidList[row][col]);
+                if (IsOdd(valueOld) && !IsOdd(value) || !IsOdd(valueOld) && IsOdd(value))
+                {
+                    sum += value;
+                    path += ", " + PyramidList[row][col];
+                }
+                else
+                {
+                    path = "";
+                    return 0;
+                }
+                valueOld = value;
+            }
+            return sum;
+        }
+        
+        private byte[] CreateBitArray(int integer, int length)
+        {
+            BitArray boolArr = new BitArray(new int[] { integer });            
+            byte[] bitArr = new byte[length];
+            for (int i = 0; i < bitArr.Length; i++)
+            {
+                bitArr[i] = boolArr[i] ? (byte)1 : (byte)0;
+            }
+            return bitArr;
+        }
+
+        private bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+    }
+}
